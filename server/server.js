5411c87ba06dd1c0ac90fc1586828e4f070264ee
farhamehrinkm <farhamehrinkm@gmail.com>
2026-06-27 21:20:08 +0530

    fix route imports

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 
 connectDB();
-
 
 const app = express();
 
